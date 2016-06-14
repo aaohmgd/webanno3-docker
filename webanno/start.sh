d86e2909717c57a37f23caf238fc36e3fd0ef7a1
Florian Kuhn <kuhn@ids-mannheim.de>
2016-06-14 17:49:43 +0200

    added docker compose base functionality. mysql still not starting

--- webanno/start.sh
+++ webanno/start.sh
@@ -1,4 +1,2 @@
 #!usr/bin/sh
-bash /usr/bin/mysqld_safe & 
-bash /opt/webanno/bin/startup.sh 
-tail -f /opt/webanno/logs/catalina.out 
+bash /opt/webanno/bin/startup.sh && tail -f /opt/webanno/logs/catalina.out 
